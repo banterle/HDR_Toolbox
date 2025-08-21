fb293b19423675e12ff336c6e99b18389a765998
Francesco Banterle <frabante@gmail.com>
2025-08-21 08:39:06 +0200

    Update demo_build_hdr.m

--- demos/demo_build_hdr.m
+++ demos/demo_build_hdr.m
@@ -13,14 +13,15 @@
 
 clear all;
 
-name_folder = 'demos/stack';
+fn = 'stack';
+name_folder = ['demos/', fn];
 format = 'jpg';
 
 if ~isfolder(name_folder)
-    name_folder = 'stack';
+    name_folder = fn;
     output_folder = 'output';
 else
-    output_folder = 'output';
+    output_folder = 'demos/output';
 end
 
 disp('1) Read a stack of LDR images');
@@ -31,6 +32,9 @@ stack_exposure = ReadLDRStackInfo(name_folder, format);
 
 disp('3) Estimate the Camera Response Function (CRF)');
 [lin_fun, ~] = DebevecCRF(stack, stack_exposure, 256);
+
+EilertsenCRF(stack, stack_exposure);
+
 h = figure(1);
 set(h, 'Name', 'The Camera Response Function (CRF)');
 plot(0:255, lin_fun(:,1), 'r', 0:255, lin_fun(:,2),'g', 0:255, lin_fun(:,3), 'b');
@@ -39,7 +43,7 @@ disp('4) Build the radiance map using the stack and stack_exposure');
 imgHDR = BuildHDR(stack, stack_exposure, 'LUT', lin_fun, 'Deb97', 'log');
 
 disp('5) Save the radiance map in the .hdr format');
-hdrimwrite(imgHDR, [output_folder,'/stack_hdr_image.exr']);
+hdrimwrite(imgHDR, [output_folder,'/stack_hdr_image.hdr']);
 
 disp('6) Show the tone mapped version of the radiance map with gamma encoding');
 h = figure(2);
